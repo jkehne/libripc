994ae3ea0d090c7318d2ab5d57b4df2ec47ff6e4
Jens Kehne <kehne@ibds.uka.de>
2012-03-13 11:32:20 +0100

    * Add license information (btw, how did the GPLv3 get in COPYING?) * Make git ignore some object files

--- clients/common.h
+++ clients/common.h
@@ -1,3 +1,19 @@
+/*  Copyright 2011, 2012 Jens Kehne
+ *  Copyright 2012 Jan Stoess, Karlsruhe Institute of Technology
+ *
+ *  LibRIPC is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the
+ *  Free Software Foundation, either version 2.1 of the License, or (at your
+ *  option) any later version.
+ *
+ *  LibRIPC is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with libRIPC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef COMMON_H_
 #define COMMON_H_
 
