4d3d35f908332f840f4c84a27d25761fe73f4b33
Marius Hillenbrand <marius.hillenbrand@kit.edu>
2012-05-04 16:17:57 +0200

    some work on some of the hooks

--- src/infiniband/trace_hooks.h
+++ src/infiniband/trace_hooks.h
@@ -18,7 +18,7 @@
 #ifndef TRACE_HOOKS_H_
 #define TRACE_HOOKS_H_
 
-#define ENABLE_IBV_TRACING
+//#define ENABLE_IBV_TRACING
 
 #ifdef ENABLE_IBV_TRACING
 
@@ -32,11 +32,19 @@ void hooked_ibv_free_device_list(struct ibv_device **list);
 const char *hooked_ibv_get_device_name(struct ibv_device *device);
 
 uint64_t hooked_ibv_get_device_guid(struct ibv_device *device);
-*/
-struct ibv_context *hooked_ibv_open_device(struct ibv_device *device);
 
-int hooked_ibv_close_device(struct ibv_context *context);
-/*
+static inline struct ibv_context *hooked_ibv_open_device(struct ibv_device *device) {
+	struct ibv_context * ctx = ibv_open_device(device);
+	// TODO tracepoint
+	return ctx;
+}
+
+static inline int hooked_ibv_close_device(struct ibv_context *context) {
+	int res = ibv_close_device(context);
+	// TODO tracepoint
+	return res;		
+}
+
 int hooked_ibv_get_async_event(struct ibv_context *context,
 			struct ibv_async_event *event);
 
@@ -53,43 +61,84 @@ int hooked_ibv_query_gid(struct ibv_context *context, uint8_t port_num,
 
 int hooked_ibv_query_pkey(struct ibv_context *context, uint8_t port_num,
 		   int index, uint16_t *pkey);
+
+static inline struct ibv_pd *hooked_ibv_alloc_pd(struct ibv_context *context) {
+	struct ibv_pd * pd = ibv_alloc_pd(context);
+	// TODO tracepoint
+	return pd;
+}
+
+static inline int hooked_ibv_dealloc_pd(struct ibv_pd *pd);
 */
-struct ibv_pd *hooked_ibv_alloc_pd(struct ibv_context *context);
+static inline struct ibv_mr *hooked_ibv_reg_mr(struct ibv_pd *pd, void *addr,
+			  size_t length, int access) {
+				  
+	struct ibv_mr * mr = hooked_ibv_reg_mr(pd, addr, length, access);
+	// TODO tracepoint
+	return mr;
+}
 
-int hooked_ibv_dealloc_pd(struct ibv_pd *pd);
+static inline int hooked_ibv_dereg_mr(struct ibv_mr *mr) {
+	int res = ibv_dereg_mr(mr);
+	// TODO tracepoint
+	return res;
+}
 
-struct ibv_mr *hooked_ibv_reg_mr(struct ibv_pd *pd, void *addr,
-			  size_t length, int access);
+static inline struct ibv_comp_channel *hooked_ibv_create_comp_channel(struct ibv_context *context) {
+	struct ibv_comp_channel * cc = ibv_create_comp_channel(context);
+	// TODO
+	return cc;
+}
 
-int hooked_ibv_dereg_mr(struct ibv_mr *mr);
+static inline int hooked_ibv_destroy_comp_channel(struct ibv_comp_channel *channel) {
+	int res = ibv_destroy_comp_channel(channel);
+	// TODO
+	return res;
+}
 
-struct ibv_comp_channel *hooked_ibv_create_comp_channel(struct ibv_context *context);
-
-int hooked_ibv_destroy_comp_channel(struct ibv_comp_channel *channel);
-
-struct ibv_cq *hooked_ibv_create_cq(struct ibv_context *context, int cqe,
+static inline struct ibv_cq *hooked_ibv_create_cq(struct ibv_context *context, int cqe,
 			     void *cq_context,
 			     struct ibv_comp_channel *channel,
-			     int comp_vector);
+			     int comp_vector) {
+	struct ibv_cq * cq = ibv_create_cq(context, cqe, cq_context, channel, comp_vector);
+	// TODO
+	return cq;
+}
 
-int hooked_ibv_resize_cq(struct ibv_cq *cq, int cqe);
+//static inline int hooked_ibv_resize_cq(struct ibv_cq *cq, int cqe);
 
-int hooked_ibv_destroy_cq(struct ibv_cq *cq);
+static inline int hooked_ibv_destroy_cq(struct ibv_cq *cq) {
+	int res = ibv_destroy_cq(cq);
+	// TODO
+	return res;
+}
 
-int hooked_ibv_get_cq_event(struct ibv_comp_channel *channel,
-		     struct ibv_cq **cq, void **cq_context);
+static inline int hooked_ibv_get_cq_event(struct ibv_comp_channel *channel,
+		     struct ibv_cq **cq, void **cq_context) {
+	int res = ibv_get_cq_event(channel, cq, cq_context);
+	// TODO tracepoint	
+	return res;
+}
 
-void hooked_ibv_ack_cq_events(struct ibv_cq *cq, unsigned int nevents);
+static inline void hooked_ibv_ack_cq_events(struct ibv_cq *cq, unsigned int nevents) {
+	ibv_ack_cq_events(cq, nevents);
+	// TODO
+}
 
 static inline int hooked_ibv_poll_cq(struct ibv_cq *cq, int num_entries, struct ibv_wc *wc)
 {
-	return cq->context->ops.poll_cq(cq, num_entries, wc);
+	int res = ibv_poll_cq(cq, num_entries, wc);
+	// TODO
+	return res;
 }
 
 static inline int hooked_ibv_req_notify_cq(struct ibv_cq *cq, int solicited_only)
 {
-	return cq->context->ops.req_notify_cq(cq, solicited_only);
+	int res = ibv_req_notify_cq(cq, solicited_only);
+	// TODO
+	return res;
 }
+
 /*
 struct ibv_srq *hooked_ibv_create_srq(struct ibv_pd *pd,
 			       struct ibv_srq_init_attr *srq_init_attr);
@@ -109,28 +158,47 @@ static inline int hooked_ibv_post_srq_recv(struct ibv_srq *srq,
 	return srq->context->ops.post_srq_recv(srq, recv_wr, bad_recv_wr);
 }
 */
-struct ibv_qp *hooked_ibv_create_qp(struct ibv_pd *pd,
-			     struct ibv_qp_init_attr *qp_init_attr);
+static inline struct ibv_qp *hooked_ibv_create_qp(struct ibv_pd *pd,
+			     struct ibv_qp_init_attr *qp_init_attr) {
+	struct ibv_qp * qp = ibv_create_qp(pd, qp_init_attr);
+	// TODO
+	return qp;
+}
 
-int hooked_ibv_modify_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr,
-		  int attr_mask);
+static inline int hooked_ibv_modify_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr,
+		  int attr_mask) {
+	int res =  ibv_modify_qp(qp, attr, attr_mask);
+	// TODO
+	return res;
+}
 
-int hooked_ibv_query_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr,
-		 int attr_mask,
-		 struct ibv_qp_init_attr *init_attr);
+static inline int hooked_ibv_query_qp(struct ibv_qp *qp, struct ibv_qp_attr *attr,
+		 int attr_mask, struct ibv_qp_init_attr *init_attr) {
+	int res = ibv_query_qp(qp, attr, attr_mask, init_attr);
+	// TODO
+	return res;
+}
 
-int hooked_ibv_destroy_qp(struct ibv_qp *qp);
+static inline int hooked_ibv_destroy_qp(struct ibv_qp *qp) {
+	int res = ibv_destroy_qp(qp);
+	// TODO
+	return res;
+}
 
 static inline int hooked_ibv_post_send(struct ibv_qp *qp, struct ibv_send_wr *wr,
 				struct ibv_send_wr **bad_wr)
 {
-	return qp->context->ops.post_send(qp, wr, bad_wr);
+	int res = ibv_post_send(qp, wr,	bad_wr);
+	// TODO
+	return res;
 }
 
 static inline int hooked_ibv_post_recv(struct ibv_qp *qp, struct ibv_recv_wr *wr,
 				struct ibv_recv_wr **bad_wr)
 {
-	return qp->context->ops.post_recv(qp, wr, bad_wr);
+	int res = ibv_post_recv(qp, wr,	bad_wr);
+	// TODO
+	return res;
 }
 /*
 struct ibv_ah *hooked_ibv_create_ah(struct ibv_pd *pd, struct ibv_ah_attr *attr);
@@ -159,12 +227,12 @@ int hooked_ibv_fork_init(void);
 #define ibv_get_device_name(...) hooked_ibv_get_device_name(__VA_ARGS__)
 
 #define ibv_get_device_guid(...) hooked_ibv_get_device_guid(__VA_ARGS__)
-*/
+
 #define ibv_open_device(...) hooked_ibv_open_device(__VA_ARGS__)
 
 #define ibv_close_device(...) hooked_ibv_close_device(__VA_ARGS__)
 
-/*
+
 #define ibv_get_async_event(...) hooked_ibv_get_async_event(__VA_ARGS__)
 
 #define ibv_ack_async_event(...) hooked_ibv_ack_async_event(__VA_ARGS__)
@@ -176,11 +244,11 @@ int hooked_ibv_fork_init(void);
 #define ibv_query_gid(...) hooked_ibv_query_gid(__VA_ARGS__) 
 
 #define ibv_query_pkey(...) hooked_ibv_query_pkey(__VA_ARGS__)
-*/
+
 #define ibv_alloc_pd(...) hooked_ibv_alloc_pd(__VA_ARGS__)
 
 #define ibv_dealloc_pd(...) hooked_ibv_dealloc_pd(__VA_ARGS__)
-
+*/
 #define ibv_reg_mr(...) hooked_ibv_reg_mr(__VA_ARGS__)
 
 #define ibv_dereg_mr(...) hooked_ibv_dereg_mr(__VA_ARGS__)
@@ -191,7 +259,7 @@ int hooked_ibv_fork_init(void);
 
 #define ibv_create_cq(...) hooked_ibv_create_cq(__VA_ARGS__)
 
-#define ibv_resize_cq(...) hooked_ibv_resize_cq(__VA_ARGS__)
+//#define ibv_resize_cq(...) hooked_ibv_resize_cq(__VA_ARGS__)
 
 #define ibv_destroy_cq(...) hooked_ibv_destroy_cq(__VA_ARGS__)
 
